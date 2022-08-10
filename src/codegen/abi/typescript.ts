53445aa0c19dd233861e9b1ad69f322faf10410a
Emmanuel <emmanuelm41@gmail.com>
2022-08-10 10:40:48 -0300

    Generate TS definitions grouping args on an object (#31)

--- src/codegen/abi/typescript.ts
+++ src/codegen/abi/typescript.ts
@@ -1,26 +1,35 @@
-import {ABI, ArrayRegex, MapRegex} from './types.js'
+import { ABI, ArrayRegex, MapRegex } from './types.js'
 
 export const genDefinitionFile = (abi: ABI) => {
-    const accountDefinition = "type Account = { address: string; private_base64: string };"
-    const baseArguments = "account: Account, value: string"
+    const accountDefinition = 'type Account = { address: string; private_base64: string };'
+    const baseArguments = 'account: Account, value: string'
 
+    const paramsITF: string[] = []
     const contractITF = abi.functions
-        .map(
-            ({ name, return: ret, args }) =>
-                `${name} : (${baseArguments}, ${args.map((arg) => `${arg.name}:${translateType(arg.type)}`).join(', ')}) => Promise<${
-                    ret.length > 0 ? `${translateType(ret[0].type)}` : 'void'
-                }>`
-        )
+        .map(({ name, return: ret, args }) => {
+            let argsField = ''
+            if (args.length > 0) {
+                paramsITF.push(
+                    `export type ${name}Args = { \n\t${args.map((arg) => `${arg.name}:${translateType(arg.type)}`).join(';\n\t')} \n}`
+                )
+
+                argsField = `, args: ${name}Args`
+            }
+
+            return `${name} : (${baseArguments}${argsField}) => Promise<${ret.length > 0 ? `${translateType(ret[0].type)}` : 'void'}>`
+        })
         .join('\n\t')
 
     const contractTypes = abi.types
         .map(
             ({ name, fields }) =>
-                `export type ${name} = { ${fields.map((field) => `${field.name}: ${translateType(field.type)}`).join('\n')} }`
+                `export type ${name} = { \n\t${fields.map((field) => `${field.name}: ${translateType(field.type)}`).join('\n\t')} \n}`
         )
         .join('\n')
 
-    return accountDefinition + '\n\nexport interface Contract{\n\t' + contractITF + '\n}\n' + contractTypes
+    return (
+        accountDefinition + '\n\nexport interface Contract{\n\t' + contractITF + '\n}\n\n' + paramsITF.join('\n') + '\n\n' + contractTypes
+    )
 }
 
 function translateType(type: string): string {
@@ -43,15 +52,15 @@ function translateType(type: string): string {
             return 'Uint8Array'
     }
 
-    if(MapRegex.test(type)){
-        const [_, valueType] = type.split("<")[1].split(">")[0].split(",")
+    if (MapRegex.test(type)) {
+        const [_, valueType] = type.split('<')[1].split('>')[0].split(',')
         const translatedValTye = translateType(valueType.trim())
 
         return `{[key:string]: ${translatedValTye}}`
     }
 
-    if(ArrayRegex.test(type)){
-        const [valueType] = type.split("<")[1].split(">")
+    if (ArrayRegex.test(type)) {
+        const [valueType] = type.split('<')[1].split('>')
         const translatedValTye = translateType(valueType.trim())
 
         return `${translatedValTye}[]`
